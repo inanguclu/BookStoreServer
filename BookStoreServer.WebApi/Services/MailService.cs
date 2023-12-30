91453cfe2cbdb7d43ee997ab8e98c20f78d2d00c
İnan güçlü <77547694+inanguclu@users.noreply.github.com>
2023-12-30 03:39:45 +0300

    .

--- BookStoreServer.WebApi/Services/MailService.cs
+++ BookStoreServer.WebApi/Services/MailService.cs
@@ -1,5 +1,31 @@
-﻿namespace BookStoreServer.WebApi.Services;
+﻿using GenericEmailService;
+using System.Net.Mail;
 
-public class MailService
+namespace BookStoreServer.WebApi.Services;
+
+public static class MailService
 {
+    public static async Task<string> SendEmailAsync(string email, string subject, string body)
+    {
+        EmailConfigurations configurations = new(
+            Smtp: "smtp.example.com",
+            Password: "password",
+            Port: 587,
+            SSL: true,
+            Html: true);
+
+        List<string> emails = new() { email };
+
+        EmailModel<Stream> model = new(
+            Configurations: configurations,
+            FromEmail: "mymail@gmail.com",
+            ToEmails: emails,
+            Subject: subject,
+            Body: body,
+            Attachments: null);
+
+
+        string response = await EmailService.SendEmailWithMailKitAsync(model);
+
+    }
 }
